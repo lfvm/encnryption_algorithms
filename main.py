7a68690b1d3df1ceab800dad8272e1f2c0c85ed2
Marco Torres <marcotorres258@outlook.com>
2022-08-30 12:37:26 -0500

    Menu

--- main.py
+++ main.py
@@ -1,54 +1,65 @@
 """
-
-Activad cripologia
+Actividad Criptología
 29/08/2022
 
 Marco Torres 
-Fernando Valdeon 
-
-
-1. Implementa el cifrado y descifrado del cesar. Se recomienda que tu programa pueda recibir una letra a usar como llave y un mensaje a encriptar/descrifrar.
-
-
-2. Implementa el cifrado y descifrado de Vigenere.
-Implementa un "one time pad". La función debe de generar la llave automaticamente cuando encripca y regresar la llave y el texto cifrado.
-
+Fernando Valdeón 
 
 """
 from ceasar_cypher.ceasar import *
-from ceasar_cypher.decrypt_file import decryptFileCeasars 
 from vigenere.vigenere import *
-from one_time_pad.one_time_pad import oneTimePad
-
-print("CESARS CYPHER")
-
-message = "hola mundo"
-key = "z"
-print(F"Orginal message: {message}")
-
-encryptedCode = encryptMessageWithCeasarMethod(key, message)
-print(f"Encripted message: {encryptedCode}")
-
-decryptedMessage = decryptMessageWithCeasarMethod(key, encryptedCode)
-print(f"Decrypted message: {decryptedMessage}")
-print("__________________________________________________________________________")
-print("\n")
-
-print("VIGENERE CYPHER")
-key = 'abct lpety'
-print(f"Orginal message: {message}")
-encryptedCode = encryptMessageWithVigenereMethod(key, message)
-print(f"Encripted message: {encryptedCode}")
-decryptedMessage = decryptMessageWithVigenereMethod(key, encryptedCode)
-print(f"Decrypted message: {decryptedMessage}")
-print("__________________________________________________________________________")
-
-print("\n")
-print("Plain text of encrypted file 1: ")
-decryptFileCeasars()
-print("__________________________________________________________________________")
+from one_time_pad.one_time_pad import *
+from ceasar_cypher.decrypt_file import decryptFileCeasars 
 
 
-print("\n")
-print("One time Pad ")
-print(oneTimePad("hola mundo"))
+# Menú
+option = -1
+while option != 0:
+
+  print("\n\n--- ENCRYPTIONS --- ")
+  print("Choose an option: ")
+  print("1. Cesar")
+  print("2. Vigenere")
+  print("3. One Time Pad")
+  print("4. Cesar Decrypt File")
+  print("5. Cifrado del Cesar")
+  print("0. End")
+  option = int(input("Option: "))
+
+    
+  if option == 1:
+    cesar_main()
+  elif option == 2:
+    vigenere_main()
+  elif option == 3:
+    one_time_pad_main()
+  elif option == 4:
+    decryptFileCeasars()
+  elif option == 5:
+    print("Cifrado del Cesar")
+  elif option == 0:
+    continue
+  else:
+    print("\nOpción incorrecta")
+    continue
+
+  input("\nEnter to continue")
+
+# print("VIGENERE CYPHER")
+# key = 'abct lpety'
+# print(f"Orginal message: {message}")
+# encryptedCode = encryptMessageWithVigenereMethod(key, message)
+# print(f"Encripted message: {encryptedCode}")
+# decryptedMessage = decryptMessageWithVigenereMethod(key, encryptedCode)
+# print(f"Decrypted message: {decryptedMessage}")
+# print("__________________________________________________________________________")
+
+# print("\n")
+# print("Plain text of encrypted file 1: ")
+# decryptFileCeasars()
+# print("__________________________________________________________________________")
+
+
+# print("\n")
+# print("One time Pad ")
+# print(oneTimePad("hola mundo"))
--- one_time_pad/one_time_pad.py
+++ one_time_pad/one_time_pad.py
@@ -1,5 +1,6 @@
 import random as r 
 from ceasar_cypher.ceasar import *
+from vigenere.vigenere import *
 
 ALPHABET_TO_INDEX = {
     "a": 0,"b": 1,"c": 2,"d": 3,"e": 4,"f": 5,"g": 6,"h": 7,
@@ -7,30 +8,41 @@ ALPHABET_TO_INDEX = {
     "s": 18,"t": 19,"u": 20,"v": 21,"w": 22,"x": 23,"y": 24,"z": 25, " ":  26
 }
 
-# Guardar en un diccionario el indice junto con su respectiva letra
 INDEX_TO_ALPHABET = { 
     0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h",
     8: "i", 9: "j", 10: "k", 11: "l", 12: "m", 13: "n", 14: "o", 15: "p", 16: "q", 17: "r",
     18: "s", 19: "t", 20: "u", 21: "v", 22: "w", 23: "x", 24: "y", 25: "z", 26 : " "
 }
 
-
-
 def oneTimePad(message: str):
-
-    encrypted = ""
+    result = ""
     keys = ""
 
-    for word in message:
+    for letter in message:
         
-        #Obtener una llave aleatoria
+        # Obtener una letra de llave aleatoria
         randomIndex = r.randint(0,26)
-        key = INDEX_TO_ALPHABET[randomIndex]
+        keyLetter = INDEX_TO_ALPHABET[randomIndex]
 
-        #Encriptar palabra 
-        encryptedWord = encryptMessageWithCeasarMethod(key,word)
+        # Encriptar letra
+        encryptedLetter = encryptMessageWithCeasarMethod(keyLetter, letter)
 
-        keys += key
-        encrypted += word
+        keys += keyLetter
+        result += encryptedLetter
         
-    return keys, encrypted
+    return keys, result
+
+
+def one_time_pad_main():
+    print("\n--- One Time Pad ---")
+
+    message = "hola mundo"
+
+    print(f"Message: {message}")
+
+    key, encryptedCode = oneTimePad(message)
+    print(f"Result Key: {key}")
+    print(f"Encrypted: {encryptedCode}")
+
+    decryptedMessage = decryptMessageWithVigenereMethod(key, encryptedCode)
+    print(f"Decrypted: {decryptedMessage}")
